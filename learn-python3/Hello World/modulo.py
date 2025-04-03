7f356a1eeccbe9545cff81d864f1fd576d4dc5eb
Junior Jimenez Encarnacion <jimenezfrankjunior@gmail.com>
2025-04-03 12:13:17 +0200

    more excercises

---
+++ learn-python3/Hello World/modulo.py
@@ -0,0 +1,11 @@
+first_order_remainder = 269 % 10
+
+print(first_order_remainder)
+
+first_order_coupon ="no"
+
+second_order_remainder = 270 % 10
+
+print(second_order_remainder)
+
+second_order_coupon ="yes"
---
+++ learn-python3/Hello World/quilts.py
@@ -0,0 +1,11 @@
+quilt_width = 8
+quilt_length = 12
+
+# Then we print the size
+print(quilt_width * quilt_length)
+
+# Whoops! That's a little too large
+quilt_length = 8
+
+# Let's see how large it is now
+print(quilt_width * quilt_length)
