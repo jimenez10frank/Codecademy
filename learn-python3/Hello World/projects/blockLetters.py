412a18e3f157250c2240a91631f2070e2101293c
Junior Jimenez Encarnacion <jimenezfrankjunior@gmail.com>
2025-04-03 13:11:15 +0200

    Completed Hello World Syllabus!

---
+++ learn-python3/Hello World/projects/blockLetters.py
@@ -0,0 +1,19 @@
+# Frank Junior Jimenez Encarnacion
+# I like playing Soccer and Progeramming
+# inital for frank
+print(" FFFFFF")
+print("F")
+print("F")
+print("FFF")
+print("F")
+print("F")
+print("F")
+
+# Initial for Junior
+print(" JJJJJ ")
+print("    J ")
+print("    J ")
+print("    J ")
+print("J   J ")
+print("J   J ")
+print(" JJJ ")
